3b2ae61df2d86c6fcc311d2eb783812e8772b324
arushi <arushimadhesiya309@gmail.com>
2024-12-18 20:01:01 +0530

    our service bg

--- src/components/headings/TertiaryHeading.jsx
+++ src/components/headings/TertiaryHeading.jsx
@@ -1,8 +1,12 @@
-function TertiaryHeading({ color, children }) {
+function TertiaryHeading({ color = "red", children }) {
   return (
     <h3
       className={`mb-5 text-4xl font-bold capitalize ${
-        color === "white" ? "text-white" : color === "red" ? "text-red-500" : ""
+        color === "white"
+          ? "text-white"
+          : color === "red"
+          ? "text-red-500"
+          : `text-${color}`
       }`}
     >
       {children}
--- src/components/trainers/Title.jsx
+++ src/components/trainers/Title.jsx
@@ -5,7 +5,7 @@ function Title() {
   return (
     <div className="text-center">
       
-      <TertiaryHeading color="red">OUR SERVICES</TertiaryHeading>
+      <TertiaryHeading >OUR SERVICES</TertiaryHeading>
       <p className="mx-auto max-w-[42ch] font-medium text-gray-300">
       GymFluencer offers 5 essential services to help you achieve your fitness goals with ease and flexibility.
       </p>
